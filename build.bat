9a5d5929428921fcf72164f6109cd7c3c429ca14
adaspera <jusciokas1@gmail.com>
2024-08-02 13:11:00 +0300

    initial commit

---
+++ build.bat
@@ -0,0 +1,11 @@
+set files=%~dp0\main\src\main.cpp
+
+set libs=%~dp0\lib\SDL2main.lib %~dp0\lib\SDL2.lib shell32.lib
+
+call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" -arch=amd64
+@echo on
+
+cl /Zi /I "include\*" %files% /link %libs% /ENTRY:mainCRTStartup /SUBSYSTEM:WINDOWS /OUT:Tetris.exe
+::call C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe -noe -c "&{Import-Module """C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"""; Enter-VsDevShell 602e473e}; cl /Zi /I '%~dp0\lib\include\*' %files% /link %libs% /ENTRY:mainCRTStartup /OUT:Tetris.exe"
+
+pause
